65ad6b0d4c1e31c4f5d279235e30ba856fade095
youjin43 <wlwlfdl3324@naver.com>
2024-11-28 09:56:54 +0900

    오른쪽 마우스로 판 이동

--- Assets/02_Script/CameraMove.cs
+++ Assets/02_Script/CameraMove.cs
@@ -2,8 +2,8 @@ using UnityEngine;
 
 public class CameraMove : MonoBehaviour
 {
-    public float panSpeed = 24f;
-    public float zoomSpeed = 10f;
+    float panSpeed = 2f;
+    float zoomSpeed = 10f;
 
     void Start()
     {
@@ -17,5 +17,19 @@ public class CameraMove : MonoBehaviour
             Camera.main.fieldOfView += (zoomSpeed * Input.GetAxis("Mouse ScrollWheel"));
             Camera.main.fieldOfView = Mathf.Clamp(Camera.main.fieldOfView, 5, 20);
         }
+
+        //오른쪽을 누르고 있으면 마우스 이동 감지 
+        if (Input.GetMouseButton(1))
+        {
+            Debug.Log("Y : " + Input.GetAxis("Mouse X"));
+            Debug.Log("X : " + Input.GetAxis("Mouse Y"));
+
+            float x = Camera.main.transform.position.x - Input.GetAxis("Mouse X") * panSpeed;
+            x = Mathf.Clamp(x, -8, 8);
+
+            float z = Camera.main.transform.position.z - Input.GetAxis("Mouse Y") * panSpeed;
+            z = Mathf.Clamp(z, -4, 10);
+            Camera.main.transform.position = new Vector3(x, Camera.main.transform.position.y, z);
+        } 
     }
 }
